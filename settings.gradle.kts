5bca12ac1604615d2638b4d423dac0e92429751b
vasvladal <vasvladal@users.noreply.github.com>
2026-09-10 20:55:15 +0300

    7. Меняю settings.gradle.kts. Ошибки продолжаются

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,15 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
         gradlePluginPortal()
+        google()
+        mavenCentral()
     }
 }
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -24,4 +20,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "RowTutorial"
 include(":app")
- 
